65be9f825891c39ff0730d51ea5cdfc13103e73f
kgoico123 <kevin.goicochea.uer@gmail.com>
2025-06-24 13:42:01 -0500

    Actualizacion View

---
+++ ProyectoDIARS/Controllers/AdministradorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDIARS.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ProyectoDIARS/Controllers/EstudianteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDIARS.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
